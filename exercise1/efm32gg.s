7937a0ffe1859fbf31c0b734c7f5e27106ffd792
Peter <peteraaser@gmail.com>
2015-02-08 11:20:06 +0100

    Added a clock functionality using LETIMER0 in EM3

--- exercise1/efm32gg.s
+++ exercise1/efm32gg.s
@@ -1,22 +1,25 @@
-  /////////////////////////////////////////////////////////////////////////////
-  //
-  // Various useful I/O addresses and definitions for EFM32GG
-  //
-  // TODO: Add more registers
-  //
-  /////////////////////////////////////////////////////////////////////////////
+ 	
 
-
+      /////////////////////////////////////////////////////////////////////////////
+      //
+      // Various useful I/O addresses and definitions for EFM32GG
+      //
+      // TODO: Add more registers
+      // TODO: Add even more registers
+      //	
+      /////////////////////////////////////////////////////////////////////////////
+     
+     
         //////////////////////////////////////////////////////////////////////
         // GPIO
-
+	
         GPIO_PA_BASE = 0x40006000
         GPIO_PB_BASE = 0x40006024
         GPIO_PC_BASE = 0x40006048
         GPIO_PD_BASE = 0x4000606c
         GPIO_PE_BASE = 0x40006090
         GPIO_PF_BASE = 0x400060b4
-
+     
         // register offsets from base address
         GPIO_CTRL     = 0x00
         GPIO_MODEL    = 0x04
@@ -27,9 +30,9 @@
         GPIO_DOUTTGL  = 0x18
         GPIO_DIN      = 0x1c
         GPIO_PINLOCKN = 0x20
-
+     
         GPIO_BASE = 0x40006100
-
+     
         // register offsets from base address
         GPIO_EXTIPSELL = 0x00
         GPIO_EXTIPSELH = 0x04
@@ -38,26 +41,28 @@
         GPIO_IEN       = 0x10
         GPIO_IF        = 0x14
         GPIO_IFC       = 0x1c
-
+	
         //////////////////////////////////////////////////////////////////////
         // CMU
-  
+     
         CMU_BASE = 0x400c8000
-
-	CMU_HFCORECLKDIV = 0x004	//HF Core clk division	
-	CMU_HFROCTRL =0x00C		
-        CMU_HFPERCLKDIV = 0x008
-        CMU_HFPERCLKEN0 = 0x044
-	CMU_OSCENCMD = 0x020
-	CMU_CMD = 0x024
-	CMU_LFCLKSEL = 0x028
-	
-
+     
+        CMU_HFCORECLKDIV	= 0x004        //HF Core clk division 
+        CMU_HFROCTRL		= 0x00C            
+        CMU_HFPERCLKDIV 	= 0x008
+	CMU_HFCORECLKEN0	= 0x40
+        CMU_HFPERCLKEN0 	= 0x044
+        CMU_OSCENCMD 		= 0x020
+        CMU_CMD 		= 0x024
+        CMU_LFCLKSEL 		= 0x028
+	CMU_LFACLKEN0 		= 0x58
+           
+     
         CMU_HFPERCLKEN0_GPIO = 13
-
+     
         //////////////////////////////////////////////////////////////////////
         // NVIC
-
+     
         ISER0 = 0xe000e100
         ISER1 = 0xe000e104
         ICER0 = 0xe000e180
@@ -73,16 +78,78 @@
         IPR1 = 0x04
         IPR2 = 0x08
         IPR3 = 0x0c
-
+	
         //////////////////////////////////////////////////////////////////////
         // EMU
-
+	
         EMU_BASE = 0x400c6000
-	EMU_MEM_CTRL = 0x004
+        EMU_MEM_CTRL = 0x004
         EMU_CTRL = 0x000
-
+     
         //////////////////////////////////////////////////////////////////////
         // System Control Block
-
+     
         SCR = 0xe000ed10
+     
+        //////////////////////////////////////////////////////////////////////
+        // DMA_CONTROL_REGISTER
+        // 
 
+        DMA_BASE                = 0x400c2000		// DMA base register
+        DMA_CTRL_BASE           = 0x008                 // Here we set the address to the data structure
+	DMA_0_CCR               = 0x1100                // DMA Channel Control Register
+
+	//////////////////////////////////////////////////////////////////////
+        // DMA_SETTINGS	
+        // 
+	
+        DMA_STRUCT_CTRL	        = 0xC0FEBFFA            // Channel settings (description in my scratchbook!)
+	DMA_SRC_LEUART0		= 0x10			// Channel source
+	DMA_SIG_XDATAV		= 0x0			// Channel signal
+	
+	//////////////////////////////////////////////////////////////////////
+        // DMA_DATA_STRUC
+	// LIVES IN SRAM
+        // ONLY HAS CHANNEL 0
+
+	DMA_STRUCTURE           = 0x20001000		// Data structure st
+        DMA_SRC_PT              = 0x0                   // source pointer
+        DMA_DEST_PT             = 0x4                   // End pointer
+        DMA_CTRL_WORD 	        = 0x8                   // CTRL word
+        DMA_USR_PT              = 0xC     		// Not used
+	
+
+        //////////////////////////////////////////////////////////////////////
+        // PRS (pg 169)
+
+	PRS_BASE 		= 0x400CC000
+	PRS_CH0_CTRL 		= 0x10			// PRS channel ctrl, see page 171
+
+	// CH0
+	PRS_CH0_CTRL_WORD	= 0x11300007		// CH0 config. Is in hex, just looks like dec...
+
+
+        //////////////////////////////////////////////////////////////////////
+        // LEUART0 (pg 497) 
+
+	LEUART0_BASE		= 0x40084000
+	LEUART0_CTRL		= 0x0
+	
+
+	//////////////////////////////////////////////////////////////////////
+        // LETIMER0
+
+	LETIMER_BASE	= 0x40082000
+	LETIMER_CTRL	= 0x0
+	LETIMER_CMD	= 0x4
+	LETIMER_IEN	= 0x2C
+	LETIMER_IF	= 0x20
+	LETIMER_IFS	= 0x24
+	LETIMER_IFC	= 0x28
+	LETIMER_COMP0	= 0x10
+
+	//////////////////////////////////////////////////////////////////////
+        // RTC
+
+	RTC_BASE 	= 0x40080000
+	RTC_CTRL	= 0x0
--- exercise1/ex1.s
+++ exercise1/ex1.s
@@ -1,16 +1,16 @@
-        .syntax unified
-  
+ .syntax unified
+ 
         .include "efm32gg.s"
-
+ 
   /////////////////////////////////////////////////////////////////////////////
   //
   // Exception vector table
   // This table contains addresses for all exception handlers
   //
   /////////////////////////////////////////////////////////////////////////////
-  
+ 
         .section .vectors
-  
+ 
         .long   stack_top               /* Top of Stack                 */
         .long   _reset                  /* Reset Handler                */
         .long   dummy_handler           /* NMI Handler                  */
@@ -27,9 +27,9 @@
         .long   dummy_handler           /* Reserved                     */
         .long   dummy_handler           /* PendSV Handler               */
         .long   dummy_handler           /* SysTick Handler              */
-
+ 
         /* External Interrupts */
-        .long   dummy_handler
+        .long   dummy_handler	//0
         .long   gpio_handler            /* GPIO even handler */
         .long   dummy_handler
         .long   dummy_handler
@@ -41,6 +41,21 @@
         .long   dummy_handler
         .long   dummy_handler
         .long   gpio_handler            /* GPIO odd handler */
+        .long   dummy_handler	//12
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler	//20
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   letimer0_handler	//26
         .long   dummy_handler
         .long   dummy_handler
         .long   dummy_handler
@@ -53,155 +68,193 @@
         .long   dummy_handler
         .long   dummy_handler
         .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-        .long   dummy_handler
-
+ 
         .section .text
-
+ 
   /////////////////////////////////////////////////////////////////////////////
   //
   // Reset handler
   // The CPU will start executing here after a reset
   //
   /////////////////////////////////////////////////////////////////////////////
-
+ 
         .globl  _reset
         .type   _reset, %function
         .thumb_func
-_reset: 
-	//Adding some aliases, assume _BASE if no other ID
-	CMU .req R4
-	GPIO_LED .req R5
-	GPIO_btn .req R6
-	GPIO .req R7
-	EMU .req R8
+_reset:
+        //Adding some aliases, assume _BASE if no other ID
+        CMU .req R4
+        GPIO_LED .req R5
+        GPIO_btn .req R6
+        GPIO .req R7
+        EMU .req R8
+        DMA .req R9
+	LETIMER .req r10
+	RTC .req r11
+       
+        ldr GPIO_LED, =GPIO_PA_BASE
+        ldr GPIO_btn, =GPIO_PC_BASE
+        ldr CMU, =CMU_BASE
+        ldr EMU, =EMU_BASE
+        ldr GPIO, =GPIO_BASE
+        ldr DMA, =DMA_BASE
+	ldr LETIMER, =LETIMER_BASE
+	ldr RTC, =RTC_BASE
+ 
+        // Enable clock for GPIO
+	mov r1, #1
+        lsl r1, r1, #CMU_HFPERCLKEN0_GPIO
+        str r1, [CMU, #CMU_HFPERCLKEN0]
+
+	// Enable LE interface clock
+	mov r1, #0x10
+	str r1, [CMU, #CMU_HFCORECLKEN0]
+       
+        // set drive strength to high (0x2)
+        mov r1, #0x2
+        str r1, [GPIO_LED, #GPIO_CTRL]
+       
+        // Set pin 8-15 output
+        mov r1, #0x55
+        orr r1, r1, r1, lsl #8
+        orr r1, r1, r1, lsl #16
+        str r1, [GPIO_LED, #GPIO_MODEH]
+ 
+        // Set 8-15 high
+        mov r1, #0x1
+        lsl r1, r2, #8
+        str r1, [GPIO_LED, #GPIO_DOUT]
+ 
+        // Set pins 0-7 to input
+        mov r1, #0x33
+        orr r1, r1, r1, lsl #8
+        orr r1, r1, r1, lsl #16
+        str r1, [GPIO_btn, #GPIO_MODEL]
+ 
+        // Enable pull-up
+        mov r1, #0xff
+        str r1, [GPIO_btn, #GPIO_DOUT]
+ 
+        // Disable SRAM
+        mov r1, #0x7
+        str r1, [EMU, #EMU_MEM_CTRL]
+ 
+        // Disabling LFACLK and LFBCLK
+	// ULFRCO still enabled for LETIMER0
+	// This also sets EM3
+        mov r1, #0x1
+	lsl r1, #0x10
+        str r1, [CMU, #CMU_LFCLKSEL]
+
+	// We enable ULFRCO for LETIMER and RTC
+	mov r1, #6
+	str r1, [CMU, #CMU_LFACLKEN0]
 	
-	
-	ldr GPIO_LED, =GPIO_PA_BASE
-	ldr GPIO_btn, =GPIO_PC_BASE
-	ldr CMU, =CMU_BASE
-	ldr EMU, =EMU_BASE
-	ldr GPIO, =GPIO_BASE
-
-	// set bit for GPIO clk
-  mov r2, #1
-  lsl r2, r2, #CMU_HFPERCLKEN0_GPIO
-  orr r1, r1, r2
-  str r1, [CMU, #CMU_HFPERCLKEN0]	
-	
-  // set drive strength to high (0x2)
-  mov r1, #0x2
-  str r1, [GPIO_LED, #GPIO_CTRL]
-        
-  // Set pin 8-15 output
-  mov r1, #0x55
-  orr r1, r1, r1, lsl #8
-  orr r1, r1, r1, lsl #16
-  str r1, [GPIO_LED, #GPIO_MODEH]
-
-  // Set 8-15 high
-  mov r1, #0x1
-  lsl r1, r2, #8
-  str r1, [GPIO_LED, #GPIO_DOUT]
-
-	// Set pins 0-7 to input
-  mov r1, #0x33
-  orr r1, r1, r1, lsl #8
-  orr r1, r1, r1, lsl #16
-  str r1, [GPIO_btn, #GPIO_MODEL]
-
-	// Enable pull-up
-  mov r1, #0xff
-  str r1, [GPIO_btn, #GPIO_DOUT]
-
-	// Disable SRAM
-	mov r1, #0x7
-	str r1, [EMU, #EMU_MEM_CTRL]
-
-	// Disabling LFACLK and LFBCLK
-	mov r1, #0x0
-	str r1, [CMU, #CMU_LFCLKSEL]
-	
-	// Initially turn off all LEDs
-	mov r1, #0xFF
-	lsl r1, #8
-	str r1, [GPIO_LED, #GPIO_DOUT]
-	
+ 
+        // Make sure lights are off
+        mov r1, #0xFF
+        lsl r1, #0x8
+        str r1, [GPIO_LED, #GPIO_DOUT]
+ 
+       
 enable_interrupt:
-	ldr r1, =0x22222222
-	str r1, [GPIO, #GPIO_EXTIPSELL]
+        ldr r1, =0x22222222
+        str r1, [GPIO, #GPIO_EXTIPSELL]
+ 
+        // Set interrupt on 1->0
+        ldr r1, =0x0ff
+        str r1, [GPIO, #GPIO_EXTIFALL]
+ 
+        // Set interrupt on 0->1
+        str r1, [GPIO, #GPIO_EXTIRISE]
+ 
+        // Enable interrupt generation
+        str r1, [GPIO, #GPIO_IEN]
+       
+        // Enable interrupt handling for GPIO odd, even and LETIMER0
+       	mov r2, #1
+	lsl r2, #26
+	ldr r3, =0x802
+	orr r2, r2, r3
+        ldr r1, =ISER0
+        str r2, [r1]
 
-	// Set interrupt on 1->0
-	ldr r1, =0x0ff
-	str r1, [GPIO, #GPIO_EXTIFALL]
+set_up_LETIMER:
 
-	// Set interrupt on 0->1
-	str r1, [GPIO, #GPIO_EXTIRISE]
+	// Setting comp0 value, i.e which val we want to count down from
+	ldr r1, =0x100
+	str r1, [LETIMER, #LETIMER_COMP0]
 
-	// Enable interrupt generation
-	str r1, [GPIO, #GPIO_IEN]
+	// We set the comp0 to be top val
+	mov r1, #1
+	lsl r1, #9
+	str r1, [LETIMER, #LETIMER_CTRL]
+
+	// We set the LETIMER to interrupt on underflow
+	mov r1, #4
+	str r1, [LETIMER, #LETIMER_IEN]
+	// str r1, [LETIMER, #LETIMER_IFS]
+
+	// We start LETIMER
+	mov r1, #1
+	str r1, [LETIMER, #LETIMER_CMD]
 	
-	// Enable interrupt handling
-	ldr r1, =ISER0
-	ldr r2, =0x802
-	str r2, [r1] 
-
-enable_sleep: 
-	// Enable deep sleep mode 2
-	ldr r1, =SCR
-	mov r2, #6
-	str r2, [r1]
+enable_sleep:
+        // Enable deep sleep mode 2
+        ldr r1, =SCR
+        mov r2, #6
+        str r2, [r1]
 
 program_loop:
-	// Go to sleep
- 	wfi
-	b program_loop  
-  
+        // Go to sleep
+        wfi
+        b program_loop  
+ 
   /////////////////////////////////////////////////////////////////////////////
   //
   // GPIO handler
   // The CPU will jump here when there is a GPIO interrupt
   //
   /////////////////////////////////////////////////////////////////////////////
-  
+ 
         .thumb_func
 gpio_handler:  
-	ldr r1, [GPIO, #GPIO_IF]	// Reads which port has flagged interrupt
-	str r1, [GPIO, #GPIO_IFC]	// Clears interrupt
+        ldr r1, [GPIO, #GPIO_IF]        // Reads which port has flagged interrupt
+        str r1, [GPIO, #GPIO_IFC]       // Clears interrupt
 	
-	// Fetch current light and button values
-	ldr r2, [GPIO_LED, GPIO_DOUT]
-	ldr r1, [GPIO_btn, GPIO_DIN]
-	
-	// Align button input and LED output
-	lsl r1, r1, #8
-	// XOR input and output to toggle leds
-	eor r1, r1, r2
-	// Invert all values because we have active low
-	mvn r1, r1
+        // Fetch current lights and buttons
+        ldr r2, [GPIO_LED, #GPIO_DOUT]
+        ldr r1, [GPIO_btn, #GPIO_DIN]
+        // Align button input and LED output   
+        lsl r1, r1, #8
+        eor r1, r1, r2
+        // XOR input and output
+        mvn r1, r1
+        str r1, [GPIO_LED, #GPIO_DOUT]
 
+        bx lr
+
+	
+
+	.thumb_func
+letimer0_handler:
+	ldr r1, [LETIMER, #LETIMER_IF] 
+	str r1, [LETIMER, #LETIMER_IFC]
+	// Count down
+	ldr r1, [GPIO_LED, #GPIO_DOUT]
+	lsr r1, #8
+	sub r1, #1
+	lsl r1, #8
 	str r1, [GPIO_LED, #GPIO_DOUT]
-	
+
 	bx lr
-
 	
-
   /////////////////////////////////////////////////////////////////////////////
-  
+ 
         .thumb_func
 dummy_handler:  
         b .  // do nothing
 
+	
+
+	
