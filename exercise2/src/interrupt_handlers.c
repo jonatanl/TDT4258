f1e3d1f47316f2b45df9ef5f1ba541af147c631d
Peter Aaser <peteraaser@gmail.com>
2015-02-19 10:31:36 +0100

    Added handler for timer1 interrupt

--- exercise2/src/interrupt_handlers.c
+++ exercise2/src/interrupt_handlers.c
@@ -6,10 +6,30 @@
 /* TIMER1 interrupt handler */
 void __attribute__ ((interrupt)) TIMER1_IRQHandler() 
 {  
-  // Blink the leftmost LED
-  *GPIO_PA_DOUT ^= 0x0100;
+  /*
+    TODO feed new samples to the DAC
+    remember to clear the pending interrupt by writing 1 to TIMER1_IFC
+  */
 
-  // clear the interrupt
+  
+  *DAC0_CH0DATA += 128;
+
+  if( *DAC0_CH0DATA >= 1050 ){
+    *DAC0_CH0DATA -= 1024;  
+  }
+  
+  
+
+  
+  // Calculate the next LEDs that display time
+   int time = *GPIO_PA_DOUT >> 8;
+  time = ~time;
+  time = (time + 1) & 0xffff;
+  time = ~time;
+  *GPIO_PA_DOUT = time << 8;
+  //
+
+  // Clear the interrupt 
   *TIMER1_IFC = 1;
 }
 
