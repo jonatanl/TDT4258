a750405620b301827daa70e79542d130e0b17609
Audun Sutterud <audunx@gmail.com>
2015-03-02 19:45:26 +0100

    Initial commit on branch board_sound_defect
      This branch contains a code sample that demonstrates a defect of the
      efm32gg board labelled 119.

--- exercise2/src/ex2.c
+++ exercise2/src/ex2.c
@@ -3,102 +3,118 @@
 
 #include "efm32gg.h"
 
-/* 
-  TODO calculate the appropriate sample period for the sound wave(s) 
-  you want to generate. The core clock (which the timer clock is derived
-  from) runs at 14 MHz by default. Also remember that the timer counter
-  registers are 16 bits.
-*/
-/* The period between sound samples, in clock cycles */
-#define   SAMPLE_PERIOD   0
+//-----------------------------------------------------------------------------
+// This code demonstrates a possible defect in the efm32gg board labelled 119:
+//
+// When the module LETIMER0 is used to drive interrupts, and the MCU uses those
+// interrupts to drive an audible square waveform to the DAC, a slight
+// variation in pitch can be heard.
+//
+// This code was simplified as much as possible to avoid bugs. It based on the
+// exercise code framework, except it uses a single file for all code.
+//-----------------------------------------------------------------------------
 
-/* Declaration of peripheral setup functions */
-void setupTimer(uint32_t period);
-void setupDAC();
-void setupNVIC();
 
-/* Your code will start executing here */
+//-----------------------------------
+// Additional register definitions
+//-----------------------------------
+
+// LETIMER registers
+#define LETIMER0_BASE   0x40082000 
+#define LETIMER0_CTRL   ((volatile uint32_t*)(LETIMER0_BASE + 0x00))
+#define LETIMER0_CMD    ((volatile uint32_t*)(LETIMER0_BASE + 0x04))
+#define LETIMER0_COMP0  ((volatile uint32_t*)(LETIMER0_BASE + 0x10))
+#define LETIMER0_IFC    ((volatile uint32_t*)(LETIMER0_BASE + 0x28))
+#define LETIMER0_IEN    ((volatile uint32_t*)(LETIMER0_BASE + 0x2c))
+
+// CMU registers used to start LETIMER clock
+#define CMU_OSCENCMD     ((volatile uint32_t*)(CMU_BASE2 + 0x020))
+#define CMU_LFACLKEN0    ((volatile uint32_t*)(CMU_BASE2 + 0x058))
+
+// Interrupt bits in ISER0
+#define IRQ_DAC			    (1 << 8)
+#define IRQ_LETIMER0		(1 << 26)
+
+
 int main(void) 
 {  
-  /* Call the peripheral setup functions */
-  setupGPIO();
-  setupDAC();
-  setupTimer(SAMPLE_PERIOD);
+  //-----------------------------------
+  // Set up the letimer
+  //-----------------------------------
+
+  // Start LFRCO that drives LFACLK
+  *CMU_OSCENCMD |= (1 << 6);
+
+  // Enable clock for the Low Energy Peripheral Interface
+  *CMU_HFCORECLKEN0 |= (1 << 4);
+
+  // Enable clock for LETIMER 
+  *CMU_LFACLKEN0 |= (1 << 2);
+
+  // Enable LETIMER0_COMP0 as top register for LETIMER
+  *LETIMER0_CTRL |= (1 << 9);
+
+  // Set COMP0 to zero to get one underflow interrupt per LFACLK cycle
+  *LETIMER0_COMP0 = 0;
+
+  // Enable interrupts for LETIMER
+  *LETIMER0_IEN |= (1 << 2);
+
+  // Start the LETIMER
+  *LETIMER0_CMD |= (1 << 0);
+
+
+  //-----------------------------------
+  // Set up the DAC
+  //-----------------------------------
+
+  // Enable DAC clock
+  *CMU_HFPERCLKEN0 |= (1 << 17);
+
+  // Enable DAC output to pin, but not ADC and ACMP
+  *DAC0_CTRL |= 0x10;
+
+  // Enable left DAC channel
+  *DAC0_CH0CTRL |= 1;
+
+ 
+  //-----------------------------------
+  // Set up the NVIC
+  //-----------------------------------
+
+  // Enable DAC interrupt generation
+  *ISER0 |= IRQ_DAC;
   
-  /* Enable interrupt handling */
-  setupNVIC();
-  
-  /* TODO for higher energy efficiency, sleep while waiting for interrupts
-     instead of infinite loop for busy-waiting
-  */
+  // Enable letimer interrupt generation
+  *ISER0 |= IRQ_LETIMER0;
+
+
+  //-----------------------------------
+  // Loop and wait for interrupts
+  //-----------------------------------
   while(1);
 
   return 0;
 }
 
-void setupNVIC()
-{
-  /* TODO use the NVIC ISERx registers to enable handling of interrupt(s)
-     remember two things are necessary for interrupt handling:
-      - the peripheral must generate an interrupt signal
-      - the NVIC must be configured to make the CPU handle the signal
-     You will need TIMER1, GPIO odd and GPIO even interrupt handling for this
-     assignment.
-  */
-}
 
-/* if other interrupt handlers are needed, use the following names: 
-   NMI_Handler
-   HardFault_Handler
-   MemManage_Handler
-   BusFault_Handler
-   UsageFault_Handler
-   Reserved7_Handler
-   Reserved8_Handler
-   Reserved9_Handler
-   Reserved10_Handler
-   SVC_Handler
-   DebugMon_Handler
-   Reserved13_Handler
-   PendSV_Handler
-   SysTick_Handler
-   DMA_IRQHandler
-   GPIO_EVEN_IRQHandler
-   TIMER0_IRQHandler
-   USART0_RX_IRQHandler
-   USART0_TX_IRQHandler
-   USB_IRQHandler
-   ACMP0_IRQHandler
-   ADC0_IRQHandler
-   DAC0_IRQHandler
-   I2C0_IRQHandler
-   I2C1_IRQHandler
-   GPIO_ODD_IRQHandler
-   TIMER1_IRQHandler
-   TIMER2_IRQHandler
-   TIMER3_IRQHandler
-   USART1_RX_IRQHandler
-   USART1_TX_IRQHandler
-   LESENSE_IRQHandler
-   USART2_RX_IRQHandler
-   USART2_TX_IRQHandler
-   UART0_RX_IRQHandler
-   UART0_TX_IRQHandler
-   UART1_RX_IRQHandler
-   UART1_TX_IRQHandler
-   LEUART0_IRQHandler
-   LEUART1_IRQHandler
-   LETIMER0_IRQHandler
-   PCNT0_IRQHandler
-   PCNT1_IRQHandler
-   PCNT2_IRQHandler
-   RTC_IRQHandler
-   BURTC_IRQHandler
-   CMU_IRQHandler
-   VCMP_IRQHandler
-   LCD_IRQHandler
-   MSC_IRQHandler
-   AES_IRQHandler
-   EBI_IRQHandler
-   EMU_IRQHandler
-*/
+void __attribute__ ((interrupt)) LETIMER0_IRQHandler() 
+{  
+  // Generate a note with a frequency of
+  //    32768 / 64 = 512
+  // 
+  // and an amplitude of 128.
+  //
+  // This tone should be pure, without any variation in
+  // pitch. However, this is not the case on all boards.
+  static int i=0;
+  i = (i+1) % 64;
+  if(i<32){
+    *DAC0_CH0DATA = 128;
+  }else{
+    *DAC0_CH0DATA = 0;
+  }
+
+  // Clear the interrupt 
+  *LETIMER0_IFC = (1 << 2);
+}
