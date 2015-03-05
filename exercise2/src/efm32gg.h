72b1bd78c8ce6e0c6adbf37c7c16475104c671f0
Audun Sutterud <audunx@gmail.com>
2015-03-05 10:37:43 +0100

    Added CMU register definitions
      CMU_OSCENCMD and CMU_LFCLKSEL.

--- exercise2/src/efm32gg.h
+++ exercise2/src/efm32gg.h
@@ -48,9 +48,11 @@
 #define CMU_BASE 0x400c8000
 
 #define CMU_HFPERCLKDIV  ((volatile uint32_t*)(CMU_BASE + 0x008))
+#define CMU_OSCENCMD     ((volatile uint32_t*)(CMU_BASE + 0x020))
+#define CMU_CMD          ((volatile uint32_t*)(CMU_BASE + 0x024))
+#define CMU_LFCLKSEL     ((volatile uint32_t*)(CMU_BASE + 0x028))
 #define CMU_HFCORECLKEN0 ((volatile uint32_t*)(CMU_BASE + 0x040))
 #define CMU_HFPERCLKEN0  ((volatile uint32_t*)(CMU_BASE + 0x044))
-#define CMU_CMD          ((volatile uint32_t*)(CMU_BASE + 0x024))
 #define CMU_LFACLKEN0    ((volatile uint32_t*)(CMU_BASE + 0x058))
 #define CMU_LFAPRESC0    ((volatile uint32_t*)(CMU_BASE + 0x068))
 
