64db8ee7051360385b219ecfc6a39ce484a1249b
Audun Sutterud <audunx@gmail.com>
2015-03-06 16:32:04 +0100

    Setup to play tetris music

--- exercise2/src/ex2.c
+++ exercise2/src/ex2.c
@@ -26,6 +26,8 @@ void setupLETIMER(uint16_t period);
 // extern variables
 synth_song_playback test_playback;
 
+synth_song tetrisSong;
+
 /* Your code will start executing here */
 int main(void) 
 {  
@@ -36,33 +38,40 @@ int main(void)
   setupLETIMER(SAMPLE_PERIOD);
 
 
-  //-------------------------
-  // Creating a random sound
-  //-------------------------
-
-  // Create notes for channel 0
-  uint16_t notes1[3] = {0x1447,0x1442,0x1443};
-
-  // Create part for channel 0
-  synth_part part1;
-  synth_create_part((synth_note*)&notes1[0], 3, 0 /* channel */, &part1);
-
-  // Create notes for channel 1
-  uint16_t notes2[3] = {0x2446,0x1441,0x1442};
-
-  // Create part for channel 1
-  synth_part part2;
-  synth_create_part((synth_note*)&notes2[0], 3, 1 /* channel */, &part2);
-
-  // Create a song
-  synth_song song;
-  synth_create_song(&part1, &part2, 125, &song); 
-
   synth_part_playback part1_playback;
   synth_part_playback part2_playback;
 
   // Create a song playback in extern variable
-  synth_create_song_playback(&song, &part1_playback, &part2_playback, SAMPLING_RATE, &test_playback);
+  synth_create_song_playback(&tetrisSong, &part1_playback, &part2_playback, SAMPLING_RATE, &test_playback);
+
+
+//  //-------------------------
+//  // Creating a random sound
+//  //-------------------------
+//
+//  // Create notes for channel 0
+//  uint16_t notes1[3] = {0x1447,0x1442,0x1443};
+//
+//  // Create part for channel 0
+//  synth_part part1;
+//  synth_create_part((synth_note*)&notes1[0], 3, 0 /* channel */, &part1);
+//
+//  // Create notes for channel 1
+//  uint16_t notes2[3] = {0x2446,0x1441,0x1442};
+//
+//  // Create part for channel 1
+//  synth_part part2;
+//  synth_create_part((synth_note*)&notes2[0], 3, 1 /* channel */, &part2);
+//
+//  // Create a song
+//  synth_song song;
+//  synth_create_song(&part1, &part2, 125, &song); 
+//
+//  synth_part_playback part1_playback;
+//  synth_part_playback part2_playback;
+//
+//  // Create a song playback in extern variable
+//  synth_create_song_playback(&song, &part1_playback, &part2_playback, SAMPLING_RATE, &test_playback);
 
 
 
