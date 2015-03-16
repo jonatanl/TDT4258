13ed094a8cd6244a4301caedcdef303750a87dc8
Audun Sutterud <audunx@gmail.com>
2015-03-16 19:01:45 +0100

    Extended the framebuffer test
      Now the file /dev/fb0 is mapped to mempry, and only every other pixel
      is set yellow.

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -11,33 +11,68 @@
 
 #include <stdint.h>    // uint16_t
 
-#define BUF_SIZE (320 * 120) // Buffer covers half the screen
+#include <sys/mman.h>  // mmap(), munmap()
+
+#include <linux/fb.h>  // struct fb_copyarea
+
+#include <sys/ioctl.h> // ioctl()
+
+#define NUM_PIXELS (320 * 240) // Number of frame buffer pixels
+#define BUF_SIZE (NUM_PIXELS * sizeof(uint16_t)) // Size of the frame buffer
 
 static int error; // Error variable
 
+
 int main(int argc, char *argv[])
 {
-  // Open framebuffer write-only
-  int fb = open("/dev/fb0", O_WRONLY);
-  if(fb == -1)
+  // Open frame buffer write-only
+  //
+  // NOTE: O_WRONLY is not sufficient for mmap()
+  int fbfd = open("/dev/fb0", O_RDWR);
+  if(fbfd == -1)
     printf("Error opening file: %d\n", errno);
 
-  // Generate an array of yellow pixels
-  uint16_t *data = malloc(sizeof(uint16_t) * BUF_SIZE);
-  for(int i=0; i<BUF_SIZE; i++){
-    data[i] = (31 << 11) + (63 << 5) + (0 << 0);
+  // Map the frame buffer file to memory
+  uint16_t *framebuf = (uint16_t*)mmap(
+      NULL,                   // kernel decides physical memory address
+      BUF_SIZE,               // frame buffer size
+      PROT_READ | PROT_WRITE, // memory is read-write
+      MAP_SHARED,             // memory is shared with other processes
+      fbfd,                     // the mapped file
+      0);
+  if(framebuf == (void*)-1)
+    printf("Error mapping file to memory: %d\n", errno);
+
+  // Color every second pixel yellow
+  for(int i=0; i<NUM_PIXELS; i+=2){
+    framebuf[i] = (31 << 11) + (63 << 5) + (0 << 0);
   }
 
-  // Write yellow pixels
-  error = write(fb, (void*)data, sizeof(uint16_t) * BUF_SIZE);
+  // Synchronize file with memory map
+  //
+  // MS_SYNC: Wait until done
+  error = msync(framebuf, BUF_SIZE, MS_SYNC);
   if(error == -1)
-    printf("Error writing to file file: %d\n", errno);
-	printf("Half the screen should be yellow now!\n");
+    printf("Error synchronizing file with memory map: %d\n", errno);
 
-  // Close framebuffer
-  error = close(fb);
+  // Refresh screen area
+  struct fb_copyarea rect;
+  rect.dx = 0;
+  rect.dy = 0;
+  rect.width = 320;
+  rect.height = 240;
+  ioctl(fbfd, 0x4680, &rect);
+
+  // Delete framebuffer mapping
+  error = munmap((void*)framebuf, BUF_SIZE);
   if(error == -1)
-    printf("Error writing to file file: %d\n", errno);
+    printf("Error deleting mapping: %d\n", errno);
 
+  // Close framebuffer file
+  error = close(fbfd);
+  if(error == -1)
+    printf("Error writing to file: %d\n", errno);
+
+  printf("Every second pixel should now be yellow. Can you still see Tux?\n");
 	exit(EXIT_SUCCESS);
 }
