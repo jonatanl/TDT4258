5e7a43353d504921303c825004b4b5fef1e48011
Audun Sutterud <audunx@gmail.com>
2015-03-26 07:17:29 +0100

    Changed the driver template
      It is now a more usable starting point for further driver development.

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -1,41 +1,30 @@
-/*
- * This is a demo Linux kernel module.
- */
-
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
 
-/*
- * template_init - function to insert this module into kernel space
- *
- * This is the first of two exported functions to handle inserting this
- * code into a running kernel
- *
- * Returns 0 if successfull, otherwise -1
- */
 
-static int __init template_init(void)
+// Initialize the gamepad module and insert it into kernel space
+//
+// Returns 0 if successfull, otherwise -1
+static int __init gamepad_init(void)
 {
 	printk("Hello World, here is your module speaking\n");
 	return 0;
 }
 
-/*
- * template_cleanup - function to cleanup this module from kernel space
- *
- * This is the second of two exported functions to handle cleanup this
- * code from a running kernel
- */
 
-static void __exit template_cleanup(void)
+// Perform cleanup and remove the gamepad module from kernel space
+static void __exit gamepad_exit(void)
 {
 	 printk("Short life for a small module...\n");
 }
 
-module_init(template_init);
-module_exit(template_cleanup);
 
-MODULE_DESCRIPTION("Small module, demo only, not very useful.");
+// Tell kernel about init and exit functions
+module_init(gamepad_init);
+module_exit(gamepad_exit);
+
+
+// Module metadata
+MODULE_DESCRIPTION("Driver module for a gamepad with eight buttons.");
 MODULE_LICENSE("GPL");
-
