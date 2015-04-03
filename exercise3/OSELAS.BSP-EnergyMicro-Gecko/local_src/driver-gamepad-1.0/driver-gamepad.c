d58c3dbbfa2b66601c60c6432f1500c39708dfba
Audun Sutterud <audunx@gmail.com>
2015-04-03 08:23:48 +0200

    Replaced definitions in efm32gg.h
      The previous definitions were obsolete, using 'volatile uint32_t*'
      where the driver would need 'void*'. All definitions was replaced with
      some new register definitions for GPIO port C.

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -8,29 +8,13 @@
 #include <linux/ioport.h> // request_mem_region, ioremap_nocache
 #include <linux/io.h>
 #include <asm-generic/uaccess.h>  // ioread32
-
+#include "efm32gg.h"
 
 #define DRIVER_NAME "driver-gamepad"
 #define DEVICE_NAME "driver-gamepad0"
 
-#define DEVICE_LENGTH 1 // Length of the device in bytes
-
-
-// GPIO memory region, port C
-#define GPIO_PC_BASE 0x40006048
-#define GPIO_PC_LENGTH 0x24
-
-// GPIO registers, port C
-#define GPIO_PC_CTRL     ((void*)(GPIO_PC_BASE + 0x00))
-#define GPIO_PC_MODEL    ((void*)(GPIO_PC_BASE + 0x04))
-#define GPIO_PC_MODEH    ((void*)(GPIO_PC_BASE + 0x08))
-#define GPIO_PC_DOUT     ((void*)(GPIO_PC_BASE + 0x0c))
-#define GPIO_PC_DOUTSET  ((void*)(GPIO_PC_BASE + 0x10))
-#define GPIO_PC_DOUTCLR  ((void*)(GPIO_PC_BASE + 0x14))
-#define GPIO_PC_DOUTTGL  ((void*)(GPIO_PC_BASE + 0x18))
-#define GPIO_PC_DIN      ((void*)(GPIO_PC_BASE + 0x1c))
-#define GPIO_PC_PINLOCKN ((void*)(GPIO_PC_BASE + 0x20))
-
+// Length of the device in bytes
+#define DEVICE_LENGTH 1
 
 // necessary function prototypes
 static int gamepad_open(struct inode *inode, struct file *filp);
