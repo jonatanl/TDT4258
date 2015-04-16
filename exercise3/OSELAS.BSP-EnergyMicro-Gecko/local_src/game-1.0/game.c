435a469dc81d8280bc19223675560c104a006100
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2015-04-16 13:23:31 +0200

    Started migrating input methods from game to input

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -29,51 +29,6 @@ void signal_handler(int signal)
   // TODO: Implement a signal handler
 }
 
-
-int init_game()
-{
-  game_debug("Initializing the game ...\n");
-
-  // Open the gamepad device read-write with nonblocking IO
-  devfd = open(DEVICE_PATH, O_RDWR | O_NONBLOCK);
-  if(devfd < 0){
-    game_error("Error opening gamepad device at %s: %s\n", DEVICE_PATH, strerror(errno));
-    return -1; // TODO: Handle error
-  }
-  game_debug("OK: Gamepad device opened at %s\n", DEVICE_PATH);
-
-  // Set signal handler for the SIGIO signal
-  error = signal(SIGIO, &signal_handler);
-  if(error == SIG_ERR){
-    game_error("Error setting a signal handler: %s\n", strerror(errno));
-    return -1;
-  }
-  game_debug("OK: Set signal handler for SIGIO\n");
-
-  // This process should receive SIGIO (and SIGURG) signals for events on the device file
-  error = fcntl(devfd, F_SETOWN, getpid());
-  if(error < 0){
-    game_error("Error setting owner of SIGIO and SIGURG signals: %s\n", strerror(errno));
-    return -1;
-  }
-  game_debug("OK: Set owner for SIGIO signals on gamepad device\n");
-
-  // Enable asynchronous notification on the device file
-  int flags = fcntl(devfd, F_GETFL) | FASYNC;
-  error = fcntl(devfd, F_SETFL, flags);
-  error = fcntl(devfd, F_SETOWN, getpid());
-  if(error < 0){
-    game_error("Error setting asynchronous notification on device: %s\n", strerror(errno));
-    return -1;
-  }
-  game_debug("OK: Enabled asynchronous notification for gamepad device\n");
-
-  // No errors
-  game_debug("DONE: No errors initializing the game\n");
-  return 0;
-}
-
-
 int close_game()
 {
   game_debug("Closing the game ...\n");
--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/input.h
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/input.h
@@ -1,3 +1,5 @@
+// Implementation of the input module
+
 // Include guard
 #ifndef _INPUT_H
 #define _INPUT_H
@@ -12,5 +14,14 @@
 
 // Return the processed gamepad input as a bitfield of actions
 uint8_t get_processed_input();
+
+struct input{
+    uint8_t shoot : 1;
+    uint8_t turn_left : 1;
+    uint8_t accelerate : 1;
+    uint8_t turn_right : 1;
+};
+
+typedef struct input input;
 
 #endif // !_INPUT_H
