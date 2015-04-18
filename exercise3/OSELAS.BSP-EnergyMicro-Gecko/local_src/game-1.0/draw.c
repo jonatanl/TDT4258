2461826df6f07b32b9bba53bea236bc5c93c8e25
PeterAaser <peteraaser@gmail.com>
2015-04-18 14:11:59 +0200

    Made draw compatible with new polygon struct

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/draw.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/draw.c
@@ -27,7 +27,7 @@
 #define FB_COLOR_BLUE  color( 0,  0, 31)
 
 // Function prototypes
-void do_draw_polyline(struct vertice* points, int n_points);
+void do_draw_polyline(uint16_t* x_coords, uint16_t* y_coords, int n_points);
 void do_draw_line(int sx, int sy, int ex, int ey);
 void draw_line_octant1(int is, int ie, int dx, int dy);
 void draw_line_octant2(int is, int ie, int dx, int dy);
@@ -112,30 +112,29 @@ static int inline get_index(int x, int y)
 }
 
 
-
 void draw_polygon(struct polygon* pol)
 {
   draw_color = FB_COLOR_GRAY;
-  do_draw_polyline(pol->vertices, pol->n_vertices);
+  do_draw_polyline(pol->x_coords, pol->y_coords, pol->n_vertices);
 }
 
 void clear_polygon(struct polygon* pol)
 {
   draw_color = FB_COLOR_BLACK;
-  do_draw_polyline(pol->vertices, pol->n_vertices);
+  do_draw_polyline(pol->x_coords, pol->y_coords, pol->n_vertices);
 }
 
-void do_draw_polyline(struct vertice* points, int n_points)
+void do_draw_polyline(uint16_t* x_coords, uint16_t* y_coords, int n_points)
 {
-  int x1 = points[0].x_pos;
-  int y1 = points[0].y_pos;
+  int x1 = x_coords[0];
+  int y1 = y_coords[0];
   int x2;
   int y2;
 
-  // Draw the polygon edges
+  // Draw the polyline edges
   for(int i=1; i<n_points; i++){
-    x2 = points[i].x_pos;
-    y2 = points[i].y_pos;
+    x2 = x_coords[i];
+    y2 = y_coords[i];
     do_draw_line(x1, y1, x2, y2);
     x1 = x2;
     y1 = y1;
