6281458026811e1e74ef8f7436547ad810f3418c
Audun Sutterud <audunx@gmail.com>
2015-04-20 15:59:39 +0200

    Fixed bugs

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/spaceship.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/spaceship.c
@@ -1,26 +1,30 @@
+#include <stdbool.h>
+
 #include "logic.h"
 
+void rotate_coordinate(ifloat* xPosition, ifloat* yPosition, ifloat xCenter, ifloat yCenter, bool clockWise);
+
 // Pre calculated sine and cosine to rotate 12 degrees clockwise
-static ifloat clockWiseSine		= float_to_ifloat(0.97815);
-static ifloat clockWiseCosine 	= float_to_ifloat(0.20791);
+static ifloat clockWiseSine		= float_to_ifloat(0.97815f);
+static ifloat clockWiseCosine 	= float_to_ifloat(0.20791f);
 
 // Pre calculated sine and cosine to rotate 12 degrees counterclockwise
-static ifloat counterWiseSine 		= float_to_ifloat(-0.20791);
-static ifloat counterWiseCosine 	= float_to_ifloat(0.97815);
+static ifloat counterWiseSine 		= float_to_ifloat(-0.20791f);
+static ifloat counterWiseCosine 	= float_to_ifloat(0.97815f);
 
 void rotate_clockwise(ship_object *spaceship) {
-	int numberOfVertices = spaceship->poly->n_vertices;
+	int numberOfVertices = spaceship->poly.n_vertices;
 
-	for (int i = 0; i < count; ++i) {
-		rotate_coordinate(&spaceship->poly->x_coords[i], &spaceship->poly->y_coords[i], spaceship->x_pos, spaceship->y_pos, true);
+	for (int i = 0; i < numberOfVertices; ++i) {
+		rotate_coordinate(&spaceship->poly.x_coords[i], &spaceship->poly.y_coords[i], spaceship->x_pos, spaceship->y_pos, true);
 	}	
 }
 
 void rotate_counterclockwise(ship_object *spaceship) {
-	int numberOfVertices = spaceship->poly->n_vertices;
+	int numberOfVertices = spaceship->poly.n_vertices;
 
-	for (int i = 0; i < count; ++i) {
-		rotate_coordinate(spaceship->poly->x_coords[i], spaceship->poly->y_coords[i], spaceship->x_pos, spaceship->y_pos, false);
+	for (int i = 0; i < numberOfVertices; ++i) {
+		rotate_coordinate(&spaceship->poly.x_coords[i], &spaceship->poly.y_coords[i], spaceship->x_pos, spaceship->y_pos, false);
 	}	
 }
 
@@ -36,12 +40,12 @@ void rotate_coordinate(ifloat* xPosition, ifloat* yPosition, ifloat xCenter, ifl
 		cosine = counterWiseCosine;
 	}
 
-	ifloat xRotated = add(subtract(multiply(cosine, xPosition),
-					multiply(sine, yPosition)),
+	ifloat xRotated = add(subtract(multiply(cosine, *xPosition),
+					multiply(sine, *yPosition)),
 					subtract(xCenter, add(multiply(xCenter, cosine), multiply(yCenter, sine))));
 
-	ifloat yRotated = add(multiply(sine, xPosition),
-					add(multiply(cosine, yPosition),
+	ifloat yRotated = add(multiply(sine, *xPosition),
+					add(multiply(cosine, *yPosition),
 					subtract(yCenter, subtract(multiply(xCenter, sine), multiply(yCenter, cosine)))));
 
 	*xPosition = xRotated;
