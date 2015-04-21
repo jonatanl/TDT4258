5a4657a2cc6ec1d5caf2b59c415717d6153a7fcb
PeterAaser <peteraaser@gmail.com>
2015-04-21 13:31:42 +0200

    Added method for checking bounding box collision

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/util.h
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/util.h
@@ -36,3 +36,5 @@
 
 #define ARG_MAX(a, b)                   (a < b ? a : b)
 #define ARG_MIN(a, b)                   (a > b ? a : b)
+
+#define INTERSECTS(p1_min, p1_max, p2_min, p2_max)   ((p1_min > p2_min && p1_min < p2_max) || (p1_max > p2_min && p1_max < p2_max))
