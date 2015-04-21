df637781826a0efcc26fa305d62837b1a765e7c9
Jonatan Lund <jonatan.lund.92@gmail.com>
2015-04-21 12:17:53 +0200

    Added comments to spaceship.c

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/spaceship.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/spaceship.c
@@ -12,14 +12,16 @@ static float clockWiseCosine 	= 0.20791f;
 static float counterWiseSine 	= -0.20791f;
 static float counterWiseCosine 	= 0.97815f;
 
+// Rotate all vertices in ship clockwise
 void rotate_clockwise(ship_object *spaceship) {
 	int numberOfVertices = spaceship->poly.n_vertices;
 
 	for (int i = 0; i < numberOfVertices; ++i) {
 		rotate_coordinate(&spaceship->poly.x_coords[i], &spaceship->poly.y_coords[i], spaceship->x_pos, spaceship->y_pos, true);
-	}	
+	}
 }
 
+// Rotate all vertices in ship counterclockwise
 void rotate_counterclockwise(ship_object *spaceship) {
 	int numberOfVertices = spaceship->poly.n_vertices;
 
@@ -28,10 +30,12 @@ void rotate_counterclockwise(ship_object *spaceship) {
 	}	
 }
 
+// Rotate a single coordinate
 void rotate_coordinate(int32_t* xPosition, int32_t* yPosition, int32_t xCenter, int32_t yCenter, bool clockWise) {
 	float sine;
 	float cosine;
 
+	// Different sine and cosine valuse depending on which way to rotate
 	if (clockWise) {
 		sine = clockWiseSine;
 		cosine = clockWiseCosine;
