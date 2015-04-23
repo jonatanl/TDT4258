ba6fcc2a97c7d39a2fa367c81a055d5d7ea98b43
Audun Sutterud <audunx@gmail.com>
2015-04-23 08:25:07 +0200

    Inspected geometry.c for errors
      Also did some restructuring.

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/geometry.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/geometry.c
@@ -8,40 +8,29 @@
 // Returns the dot product between (x1, y1) and (x2, y2)
 int32_t inline dot_product(int32_t x1, int32_t y1, int32_t x2, int32_t y2){
   return (x1 * x2) + (y1 * y2);
-} 
+}
+ 
 
-// Returns the smallest dot product between (x, y) and a point in x_coords / y_coords
-int32_t min_dot_product(int32_t x, int32_t y, int32_t* x_coords, int32_t* y_coords, int n_vertices)
+// Returns the largest and the smallest dot product between (x, y) and a point in x_coords / y_coords
+void min_max_dot_product(int32_t x, int32_t y,
+    int32_t* x_coords, int32_t* y_coords, int n_vertices, int* min, int* max)
 {
-  int32_t min = INT32_MAX;
-  int32_t product;
-
-  for(int i=0; i<n_vertices; i++){
+  int32_t product = dot_product(x, x_coords[0], y, y_coords[0]);
+  *min = product;
+  *max = product;
+  for(int i=1; i<n_vertices; i++){
     product = dot_product(x, x_coords[i], y, y_coords[i]);
-    if(min > product){
-      min = product;
-    } 
+    if(*min > product){ *min = product; } 
+    if(*max < product){ *max = product; }
   }
-  return min;
 }
 
-// Returns the largest dot product between (x, y) and a point in x_coords / y_coords
-int32_t max_dot_product(int32_t x, int32_t y, int32_t* x_coords, int32_t* y_coords, int n_vertices)
-{
-  int32_t max = INT32_MIN;
-  int32_t product;
 
-  for(int i=0; i<n_vertices; i++){
-    product = dot_product(x, x_coords[i], y, y_coords[i]);
-    if(max < product){
-      max = product;
-    } 
-  }
-  return max;
-}
-
-// Running time is O(m x log(n)) where m and n is the number of edges in each polygon.
-bool do_get_intersection_time(
+// NOTE: Running time is O(m x n) where m and n is the number of edges in each
+// polygon. This can be improved to O(m x log(n)) by implementing a binary
+// search in min_max_dot_product(). However, for small polygons the improvement
+// will be small. 
+bool get_intersection_time(
     struct polygon* poly1,  // the polygons to check for intersection
     struct polygon* poly2,  //
     int32_t x_speed1,     // the speed of the polygons
@@ -85,19 +74,25 @@ bool do_get_intersection_time(
   *time_first_max = INT32_MIN;
   *time_last_min  = INT32_MAX;
 
-  // Search through separating axes of polygon1
-  for(int i=0; i < n_vertices1; i++){
+  // Search through separating axes of polygon1 and polygon2
+  for(int i1=0, i2=0; (i1 + i2) != (n_vertices1 + n_vertices2);){
 
-    // Calculate the next separation axis
-    direction_x =  y_coords1[(i + 1) % n_vertices1] - y_coords1[i];
-    direction_y = -x_coords1[(i + 1) % n_vertices1] - x_coords1[i];
+    // Calculate the next separation axis as a normal vector to the current
+    // polygon edge. First pick from polygon1, then from polygon2.
+    if(i1 < n_vertices1){ // pick a separation axis from polygon1
+      direction_x =  y_coords1[(i1 + 1) % n_vertices1] - y_coords1[i1];
+      direction_y = -x_coords1[(i1 + 1) % n_vertices1] - x_coords1[i1];
+      i1++;
+    }else{ // pick a separation axis from polygon2
+      direction_x =  y_coords2[(i2 + 1) % n_vertices2] - y_coords2[i2];
+      direction_y = -x_coords2[(i2 + 1) % n_vertices2] - x_coords2[i2];
+      i2++;
+    }
 
     // Calculate the minimum and maximum values of the polygon vertices
     // projected onto the separation axis
-    min1 = min_dot_product(direction_x, direction_y, x_coords1, y_coords1, n_vertices1);
-    max1 = max_dot_product(direction_x, direction_y, x_coords1, y_coords1, n_vertices1);
-    min2 = min_dot_product(direction_x, direction_y, x_coords2, y_coords2, n_vertices2);
-    max2 = max_dot_product(direction_x, direction_y, x_coords2, y_coords2, n_vertices2);
+    min_max_dot_product(direction_x, direction_y, x_coords1, y_coords1, n_vertices1, &min1, &max1);
+    min_max_dot_product(direction_x, direction_y, x_coords2, y_coords2, n_vertices2, &min2, &max2);
 
     // Calculate the speed with which the projection of polygon2 moves along
     // the separation axis relative to the speed of the projection of polygon1
@@ -113,25 +108,25 @@ bool do_get_intersection_time(
       // polygon2 is on the negative side of polygon1 in the projection 
       if(speed <= 0){ return false; }
       time_first = (min1 - max2) / speed; // rounded down
-      time_last = (max1 - min2 + (speed - 1)) / speed; // rounded up
+      time_last  = (max1 - min2 + (speed - 1)) / speed; // rounded up
     }else if(max1 < min2){ 
       
       // polygon2 is on the positive side of polygon1 in the projection 
       if(speed >= 0){ return false; }
       time_first = (min2 - max1) / (-speed); // rounded down
-      time_last = (max2 - min1 + ((-speed) - 1)) / (-speed); // rounded up
+      time_last  = (max2 - min1 + ((-speed) - 1)) / (-speed); // rounded up
     }else{
       
       // polygon2 and polygon1 overlaps in the projection 
       if(speed > 0){
         time_last = (max1 - min2 + (speed - 1)) / speed; // rounded up
       }else if(speed < 0){
-        time_last = (min1 - max2 - ((-speed) - 1)) / (-speed); // rounded down
+        time_last = (max2 - min1 + ((-speed) - 1)) / (-speed); // rounded up
       }
     }
 
-    // If the first time of intersection along the current separation axis is
-    // larger, update the first time of intersection
+    // If the first time of intersection along the current separation axis is a
+    // new maximum, update the first time of intersection
     if(time_first > *time_first_max){
       *time_first_max = time_first;
       if(*time_first_max > time_max){
@@ -139,8 +134,8 @@ bool do_get_intersection_time(
       }
     }
 
-    // If the last time of intersection along the current separation axis is
-    // smaller, update the last time of intersection
+    // If the last time of intersection along the current separation axis is a
+    // new minimum, update the last time of intersection
     if(time_last < *time_last_min){
       *time_last_min = time_last;
       if(*time_first_max > *time_last_min){
@@ -148,10 +143,7 @@ bool do_get_intersection_time(
       }
     }
   }
+
+  // Return true if no separating axis was found
   return true;
-
-  // Search through separating axes for polygon2
-  for(int i=0; i<n_vertices2; i++){
-
-  }
 }
