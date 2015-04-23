d5cc0c92f46967c760f67661dfa4b80121fc1ac4
Audun Sutterud <audunx@gmail.com>
2015-04-23 07:25:27 +0200

    Restructured the code to make it more clearer
      Also added comments

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/geometry.c
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/geometry.c
@@ -41,115 +41,117 @@ int32_t max_dot_product(int32_t x, int32_t y, int32_t* x_coords, int32_t* y_coor
 }
 
 // Running time is O(m x log(n)) where m and n is the number of edges in each polygon.
-bool time_of_collision(struct asteroid* asteroid1, struct asteroid* asteroid2,
-    int time_max,    // the time interval to search is [0, time_max]
-    int* time_first, // time of the first intersection
-    int* time_last)  // time of the last intersection
+bool do_get_intersection_time(
+    struct polygon* poly1,  // the polygons to check for intersection
+    struct polygon* poly2,  //
+    int32_t x_speed1,     // the speed of the polygons
+    int32_t y_speed1,     //
+    int32_t x_speed2,     // 
+    int32_t y_speed2,     //
+    int time_max,           // the time interval to search is [0, time_max]
+    int* time_first_max,  // time of first and last intersection
+    int* time_last_min)   //
 {
-  struct asteroid* asteroid_temp;
+  // The polygon vertices
   int32_t* x_coords1;
   int32_t* y_coords1;
   int32_t n_vertices1;
   int32_t* x_coords2;
   int32_t* y_coords2;
   int32_t n_vertices2;
-  int32_t x_speed_rel;
-  int32_t y_speed_rel;
 
-  // To minimize the search time, the first polygon should be the one with the
-  // least number of edges.
-  if(asteroid1->poly.n_vertices > asteroid2->poly.n_vertices){
-    asteroid_temp = asteroid1;
-    asteroid1 = asteroid2;
-    asteroid2 = asteroid_temp;
-  }
-
-  x_coords1 = asteroid1->poly.x_coords;
-  y_coords1 = asteroid1->poly.y_coords;
-  n_vertices1 = asteroid1->poly.n_vertices;
-  x_coords2 = asteroid2->poly.x_coords;
-  y_coords2 = asteroid2->poly.y_coords;
-  n_vertices2 = asteroid2->poly.n_vertices;
-
-  // Calculate the speed with which asteroid2 is moving towards asteroid1
-  x_speed_rel = asteroid2->x_speed - asteroid1->x_speed;
-  y_speed_rel = asteroid2->y_speed - asteroid1->y_speed;
-
-  *time_first = INT32_MIN;
-  *time_last  = INT32_MAX;
-
-  int32_t edge_x;
-  int32_t edge_y;
+  // The direction that defines a separation axis
   int32_t direction_x;
   int32_t direction_y;
+
+  // The minimum and maximum boundary values of vertices projected onto the separation axis
   int32_t min1, max1;
   int32_t min2, max2;
   int32_t speed;
-  int32_t time_first_new = INT32_MIN;
-  int32_t time_last_new  = INT32_MAX;
 
-  // Search through separating axes of asteroid1
-  for(int i=0, next_i; i < n_vertices1; i++){
+  // Time of first and last intersection for a single separation axis
+  int32_t time_first = INT32_MIN;
+  int32_t time_last  = INT32_MAX;
 
-    // Calculate the next edge vector to use as separation axis
-    next_i = (i + 1) % n_vertices1;
-    edge_x = x_coords1[next_i] - x_coords1[i];
-    edge_y = y_coords1[next_i] - y_coords1[i];
+  // Store references to the polygon vertex lists for faster access
+  x_coords1   = poly1->x_coords;
+  y_coords1   = poly1->y_coords;
+  n_vertices1 = poly1->n_vertices;
+  x_coords2   = poly2->x_coords;
+  y_coords2   = poly2->y_coords;
+  n_vertices2 = poly2->n_vertices;
 
-    direction_x =  edge_y;
-    direction_y = -edge_x;
+  // Initialize results
+  *time_first_max = INT32_MIN;
+  *time_last_min  = INT32_MAX;
 
-    // Calculate boundary values of vertices projected onto the separation axis
+  // Search through separating axes of polygon1
+  for(int i=0; i < n_vertices1; i++){
+
+    // Calculate the next separation axis
+    direction_x =  y_coords1[(i + 1) % n_vertices1] - y_coords1[i];
+    direction_y = -x_coords1[(i + 1) % n_vertices1] - x_coords1[i];
+
+    // Calculate the minimum and maximum values of the polygon vertices
+    // projected onto the separation axis
     min1 = min_dot_product(direction_x, direction_y, x_coords1, y_coords1, n_vertices1);
     max1 = max_dot_product(direction_x, direction_y, x_coords1, y_coords1, n_vertices1);
     min2 = min_dot_product(direction_x, direction_y, x_coords2, y_coords2, n_vertices2);
     max2 = max_dot_product(direction_x, direction_y, x_coords2, y_coords2, n_vertices2);
 
-    // Calculate the speed of the projection along the separation axis
-    speed = dot_product(x_speed_rel, y_speed_rel, direction_x, direction_y);
+    // Calculate the speed with which the projection of polygon2 moves along
+    // the separation axis relative to the speed of the projection of polygon1
+    speed = dot_product(
+        x_speed2 - x_speed1,
+        y_speed2 - y_speed1,
+        direction_x,
+        direction_y);
 
-    // asteroid1 is to the left of asteroid2 in the projection
+    // Find the first and last time of intersection for the current separation axis
     if(max2 < min1){
-
-      if(speed <= 0 ){
-        return false;
-      }
-      time_first_new = (min1 - max2) / speed; // rounded down
-      time_last_new = (max1 - min2 + (speed - 1)) / speed; // rounded up
-    }else if(max1 < min2){
-
-      if(speed >= 0){
-        return false;
-      }
-      time_first_new = (min2 - max1) / (-speed); // rounded down
-      time_last_new = (max2 - min1 + ((-speed) - 1)) / (-speed); // rounded up
+     
+      // polygon2 is on the negative side of polygon1 in the projection 
+      if(speed <= 0){ return false; }
+      time_first = (min1 - max2) / speed; // rounded down
+      time_last = (max1 - min2 + (speed - 1)) / speed; // rounded up
+    }else if(max1 < min2){ 
+      
+      // polygon2 is on the positive side of polygon1 in the projection 
+      if(speed >= 0){ return false; }
+      time_first = (min2 - max1) / (-speed); // rounded down
+      time_last = (max2 - min1 + ((-speed) - 1)) / (-speed); // rounded up
     }else{
       
+      // polygon2 and polygon1 overlaps in the projection 
       if(speed > 0){
-        time_last_new = (max1 - min2 + (speed - 1)) / speed; // rounded up
+        time_last = (max1 - min2 + (speed - 1)) / speed; // rounded up
       }else if(speed < 0){
-        time_last_new = (min1 - max2 - ((-speed) - 1)) / (-speed); // rounded down
+        time_last = (min1 - max2 - ((-speed) - 1)) / (-speed); // rounded down
       }
     }
 
-    if(time_first_new > *time_first){
-      *time_first = time_first_new;
-      if(*time_first > time_max){
+    // If the first time of intersection along the current separation axis is
+    // larger, update the first time of intersection
+    if(time_first > *time_first_max){
+      *time_first_max = time_first;
+      if(*time_first_max > time_max){
         return false;
       }
     }
-    if(time_last_new < *time_last){
-      *time_last = time_last_new;
-      if(*time_first > *time_last){
+
+    // If the last time of intersection along the current separation axis is
+    // smaller, update the last time of intersection
+    if(time_last < *time_last_min){
+      *time_last_min = time_last;
+      if(*time_first_max > *time_last_min){
         return false;
       }
     }
   }
   return true;
 
-  // Search through separating axes for asteroid2
+  // Search through separating axes for polygon2
   for(int i=0; i<n_vertices2; i++){
 
   }
-
 }
