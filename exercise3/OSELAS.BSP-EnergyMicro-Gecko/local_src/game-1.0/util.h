a1291d8d1d4f392b2f6f2a3ab3715675635b071d
PeterAaser <peteraaser@gmail.com>
2015-04-26 11:01:14 +0200

    Added point - line intersect macro

--- exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/util.h
+++ exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/util.h
@@ -18,6 +18,7 @@
 
 // Returns true if the intervals (p1_min, p1_max) and (p2_min, p2_max) overlaps
 #define INTERSECTS(p1_min, p1_max, p2_min, p2_max)   ((p1_min >= p2_min && p1_min <= p2_max) || (p1_max >= p2_min && p1_max <= p2_max))
+#define POINT_LINE_INTERSECTS(p1, p2_min, p2_max)    ((p1 >= p2_min && p1 <= p2_max))
 
 void wrap_coordinate(int32_t* x_pos, int32_t* y_pos);
 
